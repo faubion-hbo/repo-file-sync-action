7ceb83acd286705fcef75b0570f9df04cfa27d19
Taylor Faubion <taylor.faubion@wmcontractor.com>
2022-08-01 14:12:57 -0700

    update build artifact *eye roll*

--- dist/index.js
+++ dist/index.js
@@ -17731,11 +17731,15 @@ const run = async () => {
 				}
 
 				if (executeSource) {
+					core.info(`executing file ${ source } to generate ${ dest }`)
 					mkdirSync(dirname(dest), { recursive: true })
 					const executeArgs = Object.entries(file.executeArguments).reduce(
 						(accumulator, [ key, value ]) => `${ accumulator } ${ key.replace(/[^a-z0-9]/gi, '') }='${ value }'`,
 						''
 					).trim()
+					if (executeArgs) {
+						core.info(`passing arguments ${ executeArgs }`)
+					}
 					const executeOutput = await execCmd(`./${ source } ${ executeArgs }`)
 					writeFileSync(dest, `${ executeOutput }\n`)
 				} else {
@@ -17889,6 +17893,7 @@ run()
 		core.setFailed(err.message)
 		core.debug(err)
 	})
+
 })();
 
 module.exports = __webpack_exports__;
